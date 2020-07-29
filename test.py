b3e357099dc1f3718d608a891b1eea7546a8c1b2
Gordon Sheen <gsheen2022@gmail.com>
2020-07-28 15:53:16 -1000

    removed some code

--- test.py
+++ test.py
@@ -1,22 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-name = input("your name? ")
-print("Hello,", name)
-
-"""
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
 
 r = requests.get("https://google.com")
 print(r.status_code)
-"""
+print(r.ok)
